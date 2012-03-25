489b62ec1acad397924d2c0ccb3ef74fca9bc15b
Wilfred Hughes <me@wilfred.me.uk>
2012-03-25 23:07:15 +0100

    interpreter skeleton

--- Brainfrack.java
+++ Brainfrack.java
@@ -2,6 +2,25 @@
 
 public class Brainfrack {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String printCharacterA = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ .";
+        Interpreter.evaluate(printCharacterA);
+    }
+}
+
+/* TODO: exceptions for stackoverflow, accessing memory beyond MEMORY_SIZE */
+class Interpreter {
+    final int MEMORY_SIZE = 30000;
+    
+    private char[] memory = new char[MEMORY_SIZE];
+
+    private int instructionPointer = 0;
+    private int dataPointer = 0;
+
+    public static void evaluate(String program) {
+        while (true) {
+            break;
+        }
+
+        System.out.println("Done.");
     }
 }
