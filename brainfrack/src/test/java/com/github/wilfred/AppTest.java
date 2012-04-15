6e6172f77ab0a5dd97e35327d5762f232270c6e8
Wilfred Hughes <me@wilfred.me.uk>
2012-04-15 12:40:44 +0100

    basic unit test

--- brainfrack/src/test/java/com/github/wilfred/AppTest.java
+++ brainfrack/src/test/java/com/github/wilfred/AppTest.java
@@ -4,35 +4,17 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+    public AppTest(String testName) {
+        super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testDataIncrement() {
+        Interpreter.evaluate("+");
+        assertTrue(Interpreter.memory[0] == 1);
     }
 }
