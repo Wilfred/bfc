8a49664cf730725f6f6fde10ebb3ac29585bacda
Wilfred Hughes <me@wilfred.me.uk>
2012-04-15 13:21:54 +0100

    further unit tests

--- brainfrack/src/test/java/com/github/wilfred/AppTest.java
+++ brainfrack/src/test/java/com/github/wilfred/AppTest.java
@@ -18,5 +18,22 @@ public class AppTest extends TestCase {
         interpreter.evaluate("+");
         assertEquals(1, interpreter.memory[0]);
     }
+
+    public void testDataDecrement() {
+        Interpreter interpreter = new Interpreter();
+        interpreter.evaluate("-");
+        assertEquals(-1, interpreter.memory[0]);
+    }
+
+    public void testDataIncrement() {
+        Interpreter interpreter = new Interpreter();
+        interpreter.evaluate("+");
+        assertEquals(1, interpreter.memory[0]);
+    }
+
+    public void testComment() {
+        Interpreter interpreter = new Interpreter();
+        interpreter.evaluate("I'm a comment!");
+        assertTrue(interpreter.memory[0] == 0);
     }
 }
