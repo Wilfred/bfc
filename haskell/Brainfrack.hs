7a9f35ebf73dac622ca2a2b19665d78999251e97
Wilfred Hughes <me@wilfred.me.uk>
2012-11-16 13:57:47 +0000

    adding Program as a type alias of String

--- haskell/Brainfrack.hs
+++ haskell/Brainfrack.hs
@@ -2,6 +2,8 @@ import Data.Word (Word8)
 import Data.Char (ord, chr)
 import Control.Monad (liftM)
 
+type Program = String
+
 replaceInList [] y _ = error "Index is too big for list"
 replaceInList (x:xs) y 0 = y:xs
 replaceInList (x:xs) y index = x:replaceInList xs y (index - 1)
@@ -9,7 +11,7 @@ replaceInList (x:xs) y index = x:replaceInList xs y (index - 1)
 -- this could be faster using an array instead of a list of cells
 -- todo: use word8 instead of Int for cells
 -- todo: we need to terminate at the end of the program
-evalProgram' :: String -> Int -> Int -> [Int] -> IO ()
+evalProgram' :: Program -> Int -> Int -> [Int] -> IO ()
 evalProgram' program programPointer dataPointer cells =
   case instruction of
     '>' -> evalProgram' program (programPointer+1) (dataPointer+1) cells
