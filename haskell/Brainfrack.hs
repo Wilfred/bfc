5f830685a84fbb7f1c1a3a9e302e62dd1dbe8984
Wilfred Hughes <me@wilfred.me.uk>
2012-11-16 13:51:19 +0000

    implementing + - . , in Brainfrack.hs

--- haskell/Brainfrack.hs
+++ haskell/Brainfrack.hs
@@ -1,20 +1,39 @@
 import Data.Word (Word8)
+import Data.Char (ord, chr)
+import Control.Monad (liftM)
 
 replaceInList [] y _ = error "Index is too big for list"
 replaceInList (x:xs) y 0 = y:xs
 replaceInList (x:xs) y index = x:replaceInList xs y (index - 1)
 
-
-evalProgram' :: String -> Int -> Int -> [Word8] -> IO ()
+-- this could be faster using an array instead of a list of cells
+-- todo: use word8 instead of Int for cells
+-- tod: don't throw away instructions
+evalProgram' :: String -> Int -> Int -> [Int] -> IO ()
 evalProgram' (instruction:program) programPointer dataPointer cells =
   case instruction of
-    '>' -> evalProgram' (instruction:program) programPointer (dataPointer+1) cells
-    '<' -> evalProgram' (instruction:program) programPointer (dataPointer-1) cells
-    '+' -> undefined
-    '-' -> undefined
-    '.' -> undefined
-    ',' -> undefined
+    '>' -> evalProgram' program programPointer (dataPointer+1) cells
+    '<' -> evalProgram' program programPointer (dataPointer-1) cells
+    '+' ->
+      evalProgram' program programPointer dataPointer cells'
+      where
+        updatedCell = (cells !! dataPointer) + 1
+        cells' = replaceInList cells updatedCell dataPointer
+    '-' ->
+      evalProgram' program programPointer dataPointer cells'
+      where
+        updatedCell = (cells !! dataPointer) - 1
+        cells' = replaceInList cells updatedCell dataPointer
+    '.' -> do
+      let charToPrint = chr (cells !! dataPointer)
+      putStr [charToPrint]
+      evalProgram' program programPointer dataPointer cells
+    ',' -> do
+      updatedCell <- liftM ord getChar
+      let cells' = replaceInList cells updatedCell dataPointer
+      evalProgram' program programPointer dataPointer cells
     '[' -> undefined
     ']' -> undefined
+    _ -> return ()
 
 -- evalProgram :: String -> IO ()
