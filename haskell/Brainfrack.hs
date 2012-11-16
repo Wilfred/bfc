011d7e3880995254d104289912c6085cd0c5b602
Wilfred Hughes <me@wilfred.me.uk>
2012-11-16 13:58:26 +0000

    variable name tweaks

--- haskell/Brainfrack.hs
+++ haskell/Brainfrack.hs
@@ -12,33 +12,33 @@ replaceInList (x:xs) y index = x:replaceInList xs y (index - 1)
 -- todo: use word8 instead of Int for cells
 -- todo: we need to terminate at the end of the program
 evalProgram' :: Program -> Int -> Int -> [Int] -> IO ()
-evalProgram' program programPointer dataPointer cells =
+evalProgram' program instructionPointer cellPointer cells =
   case instruction of
-    '>' -> evalProgram' program (programPointer+1) (dataPointer+1) cells
-    '<' -> evalProgram' program (programPointer+1) (dataPointer-1) cells
+    '>' -> evalProgram' program (instructionPointer+1) (cellPointer+1) cells
+    '<' -> evalProgram' program (instructionPointer+1) (cellPointer-1) cells
     '+' ->
-      evalProgram' program (programPointer+1) dataPointer cells'
+      evalProgram' program (instructionPointer+1) cellPointer cells'
       where
-        updatedCell = (cells !! dataPointer) + 1
-        cells' = replaceInList cells updatedCell dataPointer
+        updatedCell = (cells !! cellPointer) + 1
+        cells' = replaceInList cells updatedCell cellPointer
     '-' ->
-      evalProgram' program (programPointer+1) dataPointer cells'
+      evalProgram' program (instructionPointer+1) cellPointer cells'
       where
-        updatedCell = (cells !! dataPointer) - 1
-        cells' = replaceInList cells updatedCell dataPointer
+        updatedCell = (cells !! cellPointer) - 1
+        cells' = replaceInList cells updatedCell cellPointer
     '.' -> do
-      let charToPrint = chr (cells !! dataPointer)
+      let charToPrint = chr (cells !! cellPointer)
       putStr [charToPrint]
-      evalProgram' program (programPointer+1) dataPointer cells
+      evalProgram' program (instructionPointer+1) cellPointer cells
     ',' -> do
       updatedCell <- liftM ord getChar
-      let cells' = replaceInList cells updatedCell dataPointer
-      evalProgram' program (programPointer+1) dataPointer cells
+      let cells' = replaceInList cells updatedCell cellPointer
+      evalProgram' program (instructionPointer+1) cellPointer cells
     '[' -> undefined
     ']' -> undefined
     _ -> return ()
   where
-    instruction = program !! programPointer
+    instruction = program !! instructionPointer
 
 
 -- evalProgram :: String -> IO ()
