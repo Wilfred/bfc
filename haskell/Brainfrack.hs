b85c0babfe83c536039fbf31d41b92c10f822536
Wilfred Hughes <me@wilfred.me.uk>
2012-11-16 13:57:10 +0000

    we should increment programPointer instead of iterating through the instructions, so we can backtrack

--- haskell/Brainfrack.hs
+++ haskell/Brainfrack.hs
@@ -8,32 +8,35 @@ replaceInList (x:xs) y index = x:replaceInList xs y (index - 1)
 
 -- this could be faster using an array instead of a list of cells
 -- todo: use word8 instead of Int for cells
--- tod: don't throw away instructions
+-- todo: we need to terminate at the end of the program
 evalProgram' :: String -> Int -> Int -> [Int] -> IO ()
-evalProgram' (instruction:program) programPointer dataPointer cells =
+evalProgram' program programPointer dataPointer cells =
   case instruction of
-    '>' -> evalProgram' program programPointer (dataPointer+1) cells
-    '<' -> evalProgram' program programPointer (dataPointer-1) cells
+    '>' -> evalProgram' program (programPointer+1) (dataPointer+1) cells
+    '<' -> evalProgram' program (programPointer+1) (dataPointer-1) cells
     '+' ->
-      evalProgram' program programPointer dataPointer cells'
+      evalProgram' program (programPointer+1) dataPointer cells'
       where
         updatedCell = (cells !! dataPointer) + 1
         cells' = replaceInList cells updatedCell dataPointer
     '-' ->
-      evalProgram' program programPointer dataPointer cells'
+      evalProgram' program (programPointer+1) dataPointer cells'
       where
         updatedCell = (cells !! dataPointer) - 1
         cells' = replaceInList cells updatedCell dataPointer
     '.' -> do
       let charToPrint = chr (cells !! dataPointer)
       putStr [charToPrint]
-      evalProgram' program programPointer dataPointer cells
+      evalProgram' program (programPointer+1) dataPointer cells
     ',' -> do
       updatedCell <- liftM ord getChar
       let cells' = replaceInList cells updatedCell dataPointer
-      evalProgram' program programPointer dataPointer cells
+      evalProgram' program (programPointer+1) dataPointer cells
     '[' -> undefined
     ']' -> undefined
     _ -> return ()
+  where
+    instruction = program !! programPointer
+
 
 -- evalProgram :: String -> IO ()
