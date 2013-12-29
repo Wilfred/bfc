092ab92da0dc1dff9c5cec657c1dfbaf45d6217c
Wilfred Hughes <me@wilfred.me.uk>
2013-12-29 01:17:01 +0000

    skeleton C program

---
+++ c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void eval_program(char* program, int program_length) {
+    // Our cells are initialised to 0.
+    char cells[30000] = {};
+}
+
+int main() {
+    printf("hello world!\n");
+    return 0;
+}
