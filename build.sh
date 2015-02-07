641a494c06019d38241d720a8fa756230517c0fe
Wilfred Hughes <me@wilfred.me.uk>
2015-02-07 14:51:56 +0000

    Using travis to ensure Java and Haskell build.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+cd "$ROOT_DIR/java/brainfrack"
+mvn package -q
+cd "$ROOT_DIR"
+
+cd "$ROOT_DIR/haskell"
+ghc Brainfrack.hs
+cd "$ROOT_DIR"
+
