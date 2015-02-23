8685d09f48ac52f75978936ddd29b38a20619d7f
Wilfred Hughes <me@wilfred.me.uk>
2015-02-23 00:05:20 +0000

    Skeleton test framework.

---
+++ llvm/test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+TEST(Category, Name) {
+    EXPECT_EQ(1, 2);
+}
+
+// todo: link to
+// https://code.google.com/p/googletest/source/browse/trunk/src/gtest_main.cc
+// insted.
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
