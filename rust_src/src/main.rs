9e6e2442508cf012f45e319411865c4fe445cea8
Wilfred Hughes <me@wilfred.me.uk>
2015-06-21 13:28:53 +0100

    Factoring out LLVM IR emitting.

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -6,35 +6,38 @@ use std::ptr;
 
 mod bfir;
 
+unsafe fn emit_llvm_ir() {
+    // Set up a context, module and builder in that context.
+    let context = llvm::core::LLVMGetGlobalContext();
+    let module = llvm::core::LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
+    let builder = llvm::core::LLVMCreateBuilderInContext(context);
+
+    // Get the type signature for void nop(void);
+    // Then create it in our module.
+    let void = llvm::core::LLVMVoidTypeInContext(context);
+    let function_type = llvm::core::LLVMFunctionType(void, ptr::null_mut(), 0, 0);
+    let function = llvm::core::LLVMAddFunction(module, b"nop\0".as_ptr() as *const _,
+                                               function_type);
+
+    // Create a basic block in the function and set our builder to generate
+    // code in it.
+    let bb = llvm::core::LLVMAppendBasicBlockInContext(context, function,
+                                                       b"entry\0".as_ptr() as *const _);
+    llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
+
+    // Emit a `ret void` into the function
+    llvm::core::LLVMBuildRetVoid(builder);
+
+    // Dump the module as IR to stdout.
+    llvm::core::LLVMDumpModule(module);
+
+    // Clean up. Values created in the context mostly get cleaned up there.
+    llvm::core::LLVMDisposeBuilder(builder);
+    llvm::core::LLVMDisposeModule(module);
+}
+
 fn main() {
     unsafe {
-        // Set up a context, module and builder in that context.
-        let context = llvm::core::LLVMContextCreate();
-        let module = llvm::core::LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
-        let builder = llvm::core::LLVMCreateBuilderInContext(context);
-
-        // Get the type signature for void nop(void);
-        // Then create it in our module.
-        let void = llvm::core::LLVMVoidTypeInContext(context);
-        let function_type = llvm::core::LLVMFunctionType(void, ptr::null_mut(), 0, 0);
-        let function = llvm::core::LLVMAddFunction(module, b"nop\0".as_ptr() as *const _,
-                                                   function_type);
-
-        // Create a basic block in the function and set our builder to generate
-        // code in it.
-        let bb = llvm::core::LLVMAppendBasicBlockInContext(context, function,
-                                                           b"entry\0".as_ptr() as *const _);
-        llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
-
-        // Emit a `ret void` into the function
-        llvm::core::LLVMBuildRetVoid(builder);
-
-        // Dump the module as IR to stdout.
-        llvm::core::LLVMDumpModule(module);
-
-        // Clean up. Values created in the context mostly get cleaned up there.
-        llvm::core::LLVMDisposeBuilder(builder);
-        llvm::core::LLVMDisposeModule(module);
-        llvm::core::LLVMContextDispose(context);
+        emit_llvm_ir();
     }
 }
