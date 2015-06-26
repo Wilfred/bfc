39e0dcf864d64af04257e610f933965561ac1c18
Wilfred Hughes <me@wilfred.me.uk>
2015-06-27 00:01:07 +0100

    Emit putchar and getchar.

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -5,8 +5,7 @@ use llvm::core::*;
 mod bfir;
 
 unsafe fn add_c_declarations(module: &mut llvm::LLVMModule) {
-    let byte_pointer = LLVMPointerType(
-        LLVMInt8Type(), 0);
+    let byte_pointer = LLVMPointerType(LLVMInt8Type(), 0);
     
     let mut calloc_args = vec![LLVMInt32Type(), LLVMInt32Type()];
     let calloc_type = 
@@ -17,6 +16,16 @@ unsafe fn add_c_declarations(module: &mut llvm::LLVMModule) {
     let free_type = LLVMFunctionType(
         LLVMVoidType(), free_args.as_mut_ptr(), 1, 0);
     LLVMAddFunction(module, b"free\0".as_ptr() as *const _, free_type);
+
+    let mut putchar_args = vec![LLVMInt32Type()];
+    let putchar_type = LLVMFunctionType(
+        LLVMInt32Type(), putchar_args.as_mut_ptr(), 1, 0);
+    LLVMAddFunction(module, b"putchar\0".as_ptr() as *const _, putchar_type);
+
+    let mut getchar_args = vec![];
+    let getchar_type = LLVMFunctionType(
+        LLVMInt32Type(), getchar_args.as_mut_ptr(), 0, 0);
+    LLVMAddFunction(module, b"getchar\0".as_ptr() as *const _, getchar_type);
 }
 
 unsafe fn emit_llvm_ir() {
