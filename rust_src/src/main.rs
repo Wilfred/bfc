d95817ea023c7c86ae82167f713b6bafcf52836e
Wilfred Hughes <me@wilfred.me.uk>
2015-06-26 23:55:48 +0100

    Import llvm_sys items for code brevity.

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -1,41 +1,38 @@
 extern crate llvm_sys as llvm;
 
-use std::ptr;
+use llvm::core::*;
 
 mod bfir;
 
 unsafe fn add_c_declarations(module: &mut llvm::LLVMModule) {
-    let byte_pointer = llvm::core::LLVMPointerType(
-        llvm::core::LLVMInt8Type(), 0);
+    let byte_pointer = LLVMPointerType(
+        LLVMInt8Type(), 0);
     
-    let mut calloc_args = vec![llvm::core::LLVMInt32Type(),
-                           llvm::core::LLVMInt32Type()];
-    let calloc_type =
-        llvm::core::LLVMFunctionType(
-            byte_pointer, calloc_args.as_mut_ptr(), 2, 0);
-    llvm::core::LLVMAddFunction(module, b"calloc\0".as_ptr() as *const _,
-                                calloc_type);
+    let mut calloc_args = vec![LLVMInt32Type(), LLVMInt32Type()];
+    let calloc_type = 
+        LLVMFunctionType(byte_pointer, calloc_args.as_mut_ptr(), 2, 0);
+    LLVMAddFunction(module, b"calloc\0".as_ptr() as *const _, calloc_type);
 
     let mut free_args = vec![byte_pointer];
-    let free_type = llvm::core::LLVMFunctionType(
-        llvm::core::LLVMVoidType(), free_args.as_mut_ptr(), 1, 0);
-    llvm::core::LLVMAddFunction(module, b"free\0".as_ptr() as *const _, free_type);
+    let free_type = LLVMFunctionType(
+        LLVMVoidType(), free_args.as_mut_ptr(), 1, 0);
+    LLVMAddFunction(module, b"free\0".as_ptr() as *const _, free_type);
 }
 
 unsafe fn emit_llvm_ir() {
     // Set up a context, module and builder in that context.
-    let context = llvm::core::LLVMGetGlobalContext();
-    let module = llvm::core::LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
-    let builder = llvm::core::LLVMCreateBuilderInContext(context);
+    let context = LLVMGetGlobalContext();
+    let module = LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
+    let builder = LLVMCreateBuilderInContext(context);
 
     add_c_declarations(&mut *module);
 
     // Dump the module as IR to stdout.
-    llvm::core::LLVMDumpModule(module);
+    LLVMDumpModule(module);
 
     // Clean up. Values created in the context mostly get cleaned up there.
-    llvm::core::LLVMDisposeBuilder(builder);
-    llvm::core::LLVMDisposeModule(module);
+    LLVMDisposeBuilder(builder);
+    LLVMDisposeModule(module);
 }
 
 fn main() {
