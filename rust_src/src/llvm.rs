5897dc1d5151b8b484283f9b8e7637f208beb402
Wilfred Hughes <me@wilfred.me.uk>
2015-06-28 17:06:40 +0100

    Call calloc to initialise our cells.

--- rust_src/src/llvm.rs
+++ rust_src/src/llvm.rs
@@ -1,6 +1,6 @@
 use llvm_sys::core::*;
-use llvm_sys::LLVMModule;
-use llvm_sys::prelude::LLVMTypeRef;
+use llvm_sys::{LLVMModule,LLVMBasicBlock};
+use llvm_sys::prelude::{LLVMTypeRef,LLVMValueRef};
 
 use std::ffi::CString;
 
@@ -34,6 +34,34 @@ unsafe fn add_c_declarations(module: &mut LLVMModule) {
         &mut vec![], LLVMInt32Type());
 }
 
+unsafe fn add_function_call(module: &mut LLVMModule, bb: &mut LLVMBasicBlock,
+                            fn_name: &[u8], args: &mut Vec<LLVMValueRef>,
+                            name: &[u8]) {
+    let context = LLVMGetGlobalContext();
+
+    let builder = LLVMCreateBuilderInContext(context);
+    LLVMPositionBuilderAtEnd(builder, bb);
+
+    let function = LLVMGetNamedFunction(module, fn_name.as_ptr() as *const _);
+    LLVMBuildCall(builder, function, args.as_mut_ptr(),
+                  args.len() as u32, name.as_ptr() as *const _);
+
+    LLVMDisposeBuilder(builder);
+}
+
+const NUM_CELLS: u64 = 30000;
+const CELL_SIZE_IN_BYTES: u64 = 1;
+
+unsafe fn add_cells_init(module: &mut LLVMModule, bb: &mut LLVMBasicBlock) {
+    // calloc(30000, 1);
+    let mut calloc_args = vec![
+        // TODO: define LLVM_FALSE as 0.
+        LLVMConstInt(LLVMInt32Type(), NUM_CELLS, 0),
+        LLVMConstInt(LLVMInt32Type(), CELL_SIZE_IN_BYTES, 0),
+        ];
+    add_function_call(module, bb, b"calloc\0", &mut calloc_args, b"cells\0");
+}
+
 pub unsafe fn dump_ir(module_name: &str) {
     let c_mod_name = CString::new(module_name).unwrap();
     
@@ -51,6 +79,8 @@ pub unsafe fn dump_ir(module_name: &str) {
 
     let bb = LLVMAppendBasicBlockInContext(
         context, main_fn, b"entry\0".as_ptr() as *const _);
+    add_cells_init(&mut *module, &mut *bb);
+    
     LLVMPositionBuilderAtEnd(builder, bb);
     LLVMBuildRetVoid(builder);
 
