453f700b2398b84ab3538fb5eca101075bb407e6
Wilfred Hughes <me@wilfred.me.uk>
2015-06-28 16:06:55 +0100

    Move LLVM emitting to a separate file.

---
+++ rust_src/src/llvm.rs
@@ -0,0 +1,51 @@
+use llvm_sys::core::*;
+use llvm_sys::LLVMModule;
+
+unsafe fn add_c_declarations(module: &mut LLVMModule) {
+    let byte_pointer = LLVMPointerType(LLVMInt8Type(), 0);
+    
+    let mut calloc_args = vec![LLVMInt32Type(), LLVMInt32Type()];
+    let calloc_type = 
+        LLVMFunctionType(byte_pointer, calloc_args.as_mut_ptr(), 2, 0);
+    LLVMAddFunction(module, b"calloc\0".as_ptr() as *const _, calloc_type);
+
+    let mut free_args = vec![byte_pointer];
+    let free_type = LLVMFunctionType(
+        LLVMVoidType(), free_args.as_mut_ptr(), 1, 0);
+    LLVMAddFunction(module, b"free\0".as_ptr() as *const _, free_type);
+
+    let mut putchar_args = vec![LLVMInt32Type()];
+    let putchar_type = LLVMFunctionType(
+        LLVMInt32Type(), putchar_args.as_mut_ptr(), 1, 0);
+    LLVMAddFunction(module, b"putchar\0".as_ptr() as *const _, putchar_type);
+
+    let mut getchar_args = vec![];
+    let getchar_type = LLVMFunctionType(
+        LLVMInt32Type(), getchar_args.as_mut_ptr(), 0, 0);
+    LLVMAddFunction(module, b"getchar\0".as_ptr() as *const _, getchar_type);
+}
+
+pub unsafe fn dump_ir() {
+    let context = LLVMGetGlobalContext();
+    let module = LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
+    let builder = LLVMCreateBuilderInContext(context);
+
+    add_c_declarations(&mut *module);
+
+    let mut main_args = vec![];
+    let main_type = LLVMFunctionType(
+        LLVMInt32Type(), main_args.as_mut_ptr(), 0, 0);
+    let main_fn = LLVMAddFunction(module, b"main\0".as_ptr() as *const _,
+                                  main_type);
+
+    let bb = LLVMAppendBasicBlockInContext(
+        context, main_fn, b"entry\0".as_ptr() as *const _);
+    LLVMPositionBuilderAtEnd(builder, bb);
+    LLVMBuildRetVoid(builder);
+
+    // Dump the module as IR to stdout.
+    LLVMDumpModule(module);
+
+    LLVMDisposeBuilder(builder);
+    LLVMDisposeModule(module);
+}
--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -1,61 +1,11 @@
-extern crate llvm_sys as llvm;
+extern crate llvm_sys;
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::Read;
 
-use llvm::core::*;
-
 mod bfir;
-
-unsafe fn add_c_declarations(module: &mut llvm::LLVMModule) {
-    let byte_pointer = LLVMPointerType(LLVMInt8Type(), 0);
-    
-    let mut calloc_args = vec![LLVMInt32Type(), LLVMInt32Type()];
-    let calloc_type = 
-        LLVMFunctionType(byte_pointer, calloc_args.as_mut_ptr(), 2, 0);
-    LLVMAddFunction(module, b"calloc\0".as_ptr() as *const _, calloc_type);
-
-    let mut free_args = vec![byte_pointer];
-    let free_type = LLVMFunctionType(
-        LLVMVoidType(), free_args.as_mut_ptr(), 1, 0);
-    LLVMAddFunction(module, b"free\0".as_ptr() as *const _, free_type);
-
-    let mut putchar_args = vec![LLVMInt32Type()];
-    let putchar_type = LLVMFunctionType(
-        LLVMInt32Type(), putchar_args.as_mut_ptr(), 1, 0);
-    LLVMAddFunction(module, b"putchar\0".as_ptr() as *const _, putchar_type);
-
-    let mut getchar_args = vec![];
-    let getchar_type = LLVMFunctionType(
-        LLVMInt32Type(), getchar_args.as_mut_ptr(), 0, 0);
-    LLVMAddFunction(module, b"getchar\0".as_ptr() as *const _, getchar_type);
-}
-
-unsafe fn emit_llvm_ir() {
-    let context = LLVMGetGlobalContext();
-    let module = LLVMModuleCreateWithName(b"nop\0".as_ptr() as *const _);
-    let builder = LLVMCreateBuilderInContext(context);
-
-    add_c_declarations(&mut *module);
-
-    let mut main_args = vec![];
-    let main_type = LLVMFunctionType(
-        LLVMInt32Type(), main_args.as_mut_ptr(), 0, 0);
-    let main_fn = LLVMAddFunction(module, b"main\0".as_ptr() as *const _,
-                                  main_type);
-
-    let bb = LLVMAppendBasicBlockInContext(
-        context, main_fn, b"entry\0".as_ptr() as *const _);
-    LLVMPositionBuilderAtEnd(builder, bb);
-    LLVMBuildRetVoid(builder);
-
-    // Dump the module as IR to stdout.
-    LLVMDumpModule(module);
-
-    LLVMDisposeBuilder(builder);
-    LLVMDisposeModule(module);
-}
+mod llvm;
 
 /// Read the contents of the file at path, and return a string of its
 /// contents.
@@ -89,6 +39,6 @@ fn main() {
     }
     
     unsafe {
-        emit_llvm_ir();
+        llvm::dump_ir();
     }
 }
