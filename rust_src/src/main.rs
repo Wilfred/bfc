cd84ec2e3c7a244a9327c6d868ed770477e6a366
Wilfred Hughes <me@wilfred.me.uk>
2015-06-28 15:03:00 +0100

    Read files specified on the command line.

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -1,5 +1,9 @@
 extern crate llvm_sys as llvm;
 
+use std::env;
+use std::fs::File;
+use std::io::prelude::Read;
+
 use llvm::core::*;
 
 mod bfir;
@@ -53,7 +57,34 @@ unsafe fn emit_llvm_ir() {
     LLVMDisposeModule(module);
 }
 
+/// Read the contents of the file at path, and return a string of its
+/// contents.
+fn slurp(path: &str) -> Result<String, std::io::Error> {
+    let mut file = try!(File::open(path));
+    let mut contents = String::new();
+    try!(file.read_to_string(&mut contents));
+    Ok(contents)
+}
+
 fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 2 {
+        let ref file_name = args[1];
+        match slurp(&file_name) {
+            Ok(src) => {
+                println!("src: {}", src);
+            }
+            Err(e) => {
+                println!("Could not open file: {}", e);
+                std::process::exit(1);
+            }
+        }
+    } else {
+        println!("You need to provide a file to compile.");
+        println!("For example: {} foo.bf", args[0]);
+        std::process::exit(1);
+    }
+    
     unsafe {
         emit_llvm_ir();
     }
