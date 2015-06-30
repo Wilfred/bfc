53cc8a4b5179f40cf0875365c1f8fa09b2353b58
Wilfred Hughes <me@wilfred.me.uk>
2015-06-30 22:08:33 +0100

    Write an LLVM IR file unless asked to dump.

--- rust_src/src/main.rs
+++ rust_src/src/main.rs
@@ -4,7 +4,9 @@ extern crate llvm_sys;
 
 use std::env;
 use std::fs::File;
+use std::io::Write;
 use std::io::prelude::Read;
+use std::path::Path;
 
 mod bfir;
 mod llvm;
@@ -18,6 +20,19 @@ fn slurp(path: &str) -> Result<String, std::io::Error> {
     Ok(contents)
 }
 
+/// Convert "foo.bf" to "foo.ll".
+fn ll_file_name(bf_file_name: &str) -> String {
+    let mut name_parts: Vec<_> = bf_file_name.split('.').collect();
+    let parts_len = name_parts.len();
+    if parts_len > 1 {
+        name_parts[parts_len - 1] = "ll";
+    } else {
+        name_parts.push("ll");
+    }
+
+    name_parts.connect(".")
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() > 1 {
@@ -26,8 +41,8 @@ fn main() {
         let dump_bf_ir = args.len() > 2 && args[2] == "--dump-bf-ir";
         let dump_llvm = args.len() > 2 && args[2] == "--dump-llvm";
         
-        let ref file_name = args[1];
-        match slurp(&file_name) {
+        let ref file_path = args[1];
+        match slurp(&file_path) {
             Ok(src) => {
                 let instrs = bfir::parse(&src);
                 if dump_bf_ir {
@@ -37,14 +52,25 @@ fn main() {
                 }
 
                 unsafe {
-                    let llvm_ir_raw = llvm::dump_ir(&file_name);
-                    let llvm_ir = String::from_utf8_lossy(llvm_ir_raw.as_bytes());
+                    let llvm_ir_raw = llvm::dump_ir(&file_path);
 
                     if dump_llvm {
+                        let llvm_ir = String::from_utf8_lossy(llvm_ir_raw.as_bytes());
                         println!("{}", llvm_ir);
                     } else {
                         // TODO: write to a temp file then call llc.
-                        println!("Write!");
+                        let bf_name = Path::new(file_path).file_name().unwrap();
+                        let ll_name = ll_file_name(bf_name.to_str().unwrap());
+                        match File::create(&ll_name) {
+                            Ok(mut f) => {
+                                let _ = f.write(llvm_ir_raw.as_bytes());
+                                println!("Wrote {}", ll_name);
+                            }
+                            Err(e) => {
+                                println!("Could not create file: {}", e);
+                                std::process::exit(1);
+                            }
+                        }
                     }
                 }
 
