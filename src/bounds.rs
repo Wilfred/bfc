9e116e0ccae7423749d79f72b53129a3963f4727
Wilfred Hughes <me@wilfred.me.uk>
2015-07-21 23:46:20 +0100

    Smarter bound analysis.

--- src/bounds.rs
+++ src/bounds.rs
@@ -4,15 +4,49 @@ use bfir::Instruction;
 #[allow(unused_imports)]
 use bfir::parse;
 
-const MAX_CELLS: u64 = 30000;
+const MAX_CELLS: i64 = 30000;
 
-// TODO: static analysis of cell bounds.
-pub fn highest_cell_index(_: &Vec<Instruction>) -> u64 {
-    MAX_CELLS
+pub fn highest_cell_index(instrs: &Vec<Instruction>) -> u64 {
+    let mut max_seen: i64 = 1;
+
+    // TODO: smarter handling of loops, smarter handling of pointer
+    // decrement.
+    for instr in instrs {
+        match instr {
+            &Instruction::Loop(_) => {
+                max_seen = MAX_CELLS;
+                break;
+            },
+            &Instruction::PointerIncrement(amount) => {
+                max_seen += amount as i64;
+            },
+            _ => {}
+        }
+    }
+
+    max_seen as u64
+}
+
+#[test]
+fn one_cell_bounds() {
+    let instrs = parse("+-.,").unwrap();
+    assert_eq!(highest_cell_index(&instrs), 1);
+}
+
+#[test]
+fn ptr_increment_bounds() {
+    let instrs = parse(">").unwrap();
+    assert_eq!(highest_cell_index(&instrs), 2);
+}
+
+#[test]
+fn multiple_ptr_increment_bounds() {
+    let instrs = vec![Instruction::PointerIncrement(2)];
+    assert_eq!(highest_cell_index(&instrs), 3);
 }
 
 #[test]
 fn unbounded_should_return_max() {
     let instrs = parse("[>]").unwrap();
-    assert_eq!(highest_cell_index(&instrs), MAX_CELLS);
+    assert_eq!(highest_cell_index(&instrs), MAX_CELLS as u64);
 }
