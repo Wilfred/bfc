851e23fb75d4df9c43f30c6d68c0fe0f6ee2e2cd
Wilfred Hughes <me@wilfred.me.uk>
2015-08-05 19:01:30 +0100

    Improving readability of LLVM backend tests.

--- src/llvm_tests.rs
+++ src/llvm_tests.rs
@@ -4,6 +4,24 @@ use std::ffi::CString;
 #[test]
 fn compile_empty_program() {
     let result = compile_to_ir("foo", &vec![], 10);
-    let expected = "; ModuleID = \'foo\'\n\ndeclare i8* @calloc(i32, i32)\n\ndeclare void @free(i8*)\n\ndeclare i32 @putchar(i32)\n\ndeclare i32 @getchar()\n\ndefine i32 @main() {\nentry:\n  %cells = call i8* @calloc(i32 10, i32 1)\n  %cell_index_ptr = alloca i32\n  store i32 0, i32* %cell_index_ptr\n  call void @free(i8* %cells)\n  ret i32 0\n}\n";
+    let expected = "; ModuleID = \'foo\'
+
+declare i8* @calloc(i32, i32)
+
+declare void @free(i8*)
+
+declare i32 @putchar(i32)
+
+declare i32 @getchar()
+
+define i32 @main() {
+entry:
+  %cells = call i8* @calloc(i32 10, i32 1)
+  %cell_index_ptr = alloca i32
+  store i32 0, i32* %cell_index_ptr
+  call void @free(i8* %cells)
+  ret i32 0
+}
+";
     assert_eq!(result, CString::new(expected).unwrap());
 }
