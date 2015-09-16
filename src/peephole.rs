Wilfred/bfc

commit 55e3bf703c70e423bef5d69653555af5e9d9e724

Fix syntax error.

src/peephole.rs
@@ -64,7 +64,10 @@ pub fn combine_increments(instrs: Vec<Instruction>) -> Vec<Instruction> {
         return Err((prev_instr, instr));
     }).filter(|instr| {
         // Remove any increments of 0.
-        instr != &Increment{ amount: Wrapping(0), .. }
+        if let &Increment{ amount: Wrapping(0), .. } = instr {
+            return true;
+        }
+        false
     }).map(|instr| {
         // Combine increments in nested loops too.
         match instr {

commit b8ee7aa54003b8fc862b0c218198bc3b55cce2d3

Fixing incorrect boolean broken in 55e3bf703c70e4.

src/peephole.rs
@@ -112,9 +112,9 @@ pub fn combine_increments(instrs: Vec<Instruction>) -> Vec<Instruction> {
     }).filter(|instr| {
         // Remove any increments of 0.
         if let &Increment{ amount: Wrapping(0), .. } = instr {
-            return true;
+            return false;
         }
-        false
+        true
     }).map(|instr| {
         // Combine increments in nested loops too.
         match instr {