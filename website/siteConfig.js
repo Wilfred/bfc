81163c8547ebf8ae9e5d0b3a5e0f362c658e8011
Wilfred Hughes <me@wilfred.me.uk>
2020-11-28 22:27:38 -0800

    Fix opengraph image paths

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -51,8 +51,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: "img/undraw_online.svg",
-  twitterImage: "img/undraw_tweetstorm.svg",
+  ogImage: "img/logo.png",
+  twitterImage: "img/logo.png",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
