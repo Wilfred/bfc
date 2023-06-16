5d58aa32f2aa5987264a42c9485e3006751f3b1d
Wilfred Hughes <me@wilfred.me.uk>
2023-06-15 21:02:32 -0700

    Simplify shelling out helpers

--- src/shell.rs
+++ src/shell.rs
@@ -3,15 +3,14 @@
 
 use std::process::Command;
 
-/// Execute the CLI command specified. If the command succeeds,
-/// returns stdout.
+/// Execute the CLI command specified.
 ///
 /// # Failures
 ///
 /// If the command isn't on $PATH, returns Err with a helpful
 /// message. If the command returns a non-zero exit code, returns Err
 /// with stderr.
-fn shell_command(command: &str, args: &[&str]) -> Result<String, String> {
+pub fn run_shell_command(command: &str, args: &[&str]) -> Result<(), String> {
     let mut c = Command::new(command);
     for arg in args {
         c.arg(arg);
@@ -20,21 +19,12 @@ fn shell_command(command: &str, args: &[&str]) -> Result<String, String> {
     match c.output() {
         Ok(result) => {
             if result.status.success() {
-                let stdout = String::from_utf8_lossy(&result.stdout);
-                Ok((*stdout).to_owned())
+                Ok(())
             } else {
                 let stderr = String::from_utf8_lossy(&result.stderr);
                 Err((*stderr).to_owned())
             }
         }
         Err(_) => Err(format!("Could not execute '{}'. Is it on $PATH?", command)),
-    }
-}
-
-/// Execute a CLI command as `shell_command`, but ignore stdout.
-pub fn run_shell_command(command: &str, args: &[&str]) -> Result<(), String> {
-    match shell_command(command, args) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e),
     }
 }
